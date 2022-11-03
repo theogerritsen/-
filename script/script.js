1e5b6812858822d1868c28515425a925a486a514
Theo Gerritsen <theo.gerritsen0703@gmail.com>
2022-11-03 18:24:00 +0100

    scroll

--- script/script.js
+++ script/script.js
@@ -1,12 +1,43 @@
-let btn = document.querySelector("#btn");
-let sidebar = document.querySelector(".sidebar");
-let searchBtn = document.querySelector(".bx-search");
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
 
-btn.onclick = function() {
-    sidebar.classList.toggle("active");
-}
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
 
-searchBtn.onclick = function() {
-    sidebar.classList.toggle("active");
+let card1 = document.getElementById('card1');
+let card2 = document.getElementById('card2');
+let card3 = document.getElementById('card3');
+let card4 = document.getElementById('card4');
+
+card1.onclick = function() {
+    $('html,body').animate({
+        scrollTop: $("#proj1").offset().top},
+        'slow'
+    );
 }
-    
+card2.onclick = function() {
+    $('html,body').animate({
+        scrollTop: $("#proj2").offset().top},
+        'slow'
+    );
+}
+card3.onclick = function() {
+    $('html,body').animate({
+        scrollTop: $("#proj3").offset().top},
+        'slow'
+    );
+}
+card4.onclick = function() {
+    $('html,body').animate({
+        scrollTop: $("#proj4").offset().top},
+        'slow'
+    );
+}
