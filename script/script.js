bf8a317ea8c4c932461454db8201b19f8b742978
Theo Gerritsen <theo.gerritsen0703@gmail.com>
2022-11-13 19:00:25 +0100

    update project

--- script/script.js
+++ script/script.js
@@ -16,6 +16,7 @@ let card1 = document.getElementById('card1');
 let card2 = document.getElementById('card2');
 let card3 = document.getElementById('card3');
 let card4 = document.getElementById('card4');
+let git1 = document.getElementById('git1');
 
 card1.onclick = function() {
     $('html,body').animate({
@@ -40,4 +41,8 @@ card4.onclick = function() {
         scrollTop: $("#proj4").offset().top},
         'slow'
     );
+}
+
+function getRepo(id) {
+    alert(id)
 }
